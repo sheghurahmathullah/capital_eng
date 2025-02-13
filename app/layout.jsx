4f416e9e1ab325a89356829d87cfa9e2db9cdb93
sheghurahmathullah <sheghu379@gmail.com>
2025-02-13 14:57:56 +0530

    clarity integartion

--- app/layout.jsx
+++ app/layout.jsx
@@ -59,6 +59,17 @@ export default function RootLayout({ children }) {
     gtag('config', 'G-TM7KY7962Z');`
   }
 </Script>
+
+<Script type="text/javascript">
+   {
+    ` (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "q93kym975x");`
+   }
+</Script>
+
         </head>
         <ParallaxProvider>{children}</ParallaxProvider>
         <ScrollTop />
