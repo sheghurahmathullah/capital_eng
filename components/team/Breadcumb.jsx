0928713d220f40a292d4c73161f2cccb14437870
Aniruth27 <aniruth2k3@gmail.com>
2025-02-27 22:19:52 +0530

    Header and footer colour

--- components/team/Breadcumb.jsx
+++ components/team/Breadcumb.jsx
@@ -4,7 +4,7 @@ export default function Breadcumb() {
   return (
     <div
       className="breadcumb-wrapper"
-      style={{ backgroundImage: "url(/assets/img/bg/breadcumb-bg1-3.jpg)" }}
+      style={{ backgroundImage: "url(/assets/img/team/hero-1.jpg)" }}
     >
       <div className="container">
         <div className="breadcumb-content">
